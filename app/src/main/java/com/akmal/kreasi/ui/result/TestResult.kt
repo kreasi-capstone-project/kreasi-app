f94b9924d281265e2db5d19190d824d60e9cf5d9
Akmaloktav <akmaloktavian139@gmail.com>
2024-12-13 16:53:39 +0700

    integration API create feature Recommendation

--- app/src/main/java/com/akmal/kreasi/ui/result/TestResult.kt
+++ app/src/main/java/com/akmal/kreasi/ui/result/TestResult.kt
@@ -17,23 +17,20 @@ class TestResult : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.title = "Recommendation Learning"
 
-        // Retrieve data from intent
         val recommendation = intent.getParcelableExtra<PostResponse>("RECOMMENDATION_DATA")
         if (recommendation?.predictedCourses != null) {
             setupExpandableListView(recommendation)
         } else {
             Toast.makeText(this, "Recommendation data is unavailable", Toast.LENGTH_SHORT).show()
-            finish() // Close the activity if data is missing
+            finish()
         }
     }
 
     private fun setupExpandableListView(recommendation: PostResponse) {
         val data = HashMap<String, List<String>>()
 
-        // Map user_level as a header
         data["Your Level: ${recommendation.userLevel}"] = listOf()
 
-        // Map predicted courses as items under a category
         data["Recommended Courses"] = recommendation.predictedCourses ?: listOf("No courses available")
 
         val titles = ArrayList(data.keys)
